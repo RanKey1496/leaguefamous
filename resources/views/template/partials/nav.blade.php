7b37ee3c248eade4b5c9fa122a87fde3ae5ef41c
JerrySun <jerrysun88@gmail.com>
2016-06-26 01:12:14 -0700

    Basic Nav Bar
    Added it.

---
+++ resources/views/template/partials/nav.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        League Famous
+      </a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Log In or Sign Up</a></li>
+    </ul>
+  </div>
+</nav>
