fdb3588336a7eea1f4d1e1ec84261246f5739e46
JerrySun <jerrysun88@gmail.com>
2016-07-22 15:56:52 -0700

    Summoner css

--- resources/views/summoner/summoner.blade.php
+++ resources/views/summoner/summoner.blade.php
@@ -107,10 +107,6 @@
 		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 		  <div class="modal-dialog">
 		    <div class="modal-content">
-		      <div class="modal-header">
-		        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-		        <h4 class="modal-title" id="myModalLabel">Replies (0) !!FIX!!</h4>
-		      </div>
 		      <div class="comment-body">
 				<div id="comment_{{ $comment->id }}">
 					<div class="comment-header text-left">
